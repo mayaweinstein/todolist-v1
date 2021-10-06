b1a3ea6d97504d719bb3f2c06e952a9c06318002
mayaweinstein <43150383+mayaweinstein@users.noreply.github.com>
2021-10-06 14:03:16 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+let items = [];
+let workItems = [];
+
+app.get("/", function(req, res){
+
+  var day = date.getDate();
+
+  res.render("list", {listTitle: day, items: items});
+
+})
+
+app.post("/", function(req, res){
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+})
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", items: workItems});
+})
+
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000.");
+})
